6428f13d0ee65a40407727a0e16bf40cc54ec402
saturnaliam <luciacdev@gmail.com>
2024-06-22 22:09:32 -0500

    controlboard yippeeee

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -2,4 +2,5 @@ package frc.robot;
 
 public class Ports {
     public static int DRIVER_PORT = 0;
+    public static int OPERATOR_PORT = 1;
 }
--- src/main/java/frc/robot/controller/Controls.java
+++ src/main/java/frc/robot/controller/Controls.java
@@ -7,6 +7,7 @@ public class Controls {
     private static Controls instance = null;
     
     public final CustomController driver;
+    public final CustomController operator;
 
     public static Controls getInstance() {
         if (instance == null) {
@@ -18,5 +19,45 @@ public class Controls {
     
     private Controls() {
         driver = new CustomController(Ports.DRIVER_PORT);
+        operator = new CustomController(Ports.OPERATOR_PORT);
+    }
+
+    public void update() {
+        driver.update();
+        operator.update();
+    }
+
+    // driver controls
+    public boolean goRed() {
+        return driver.aButton.isBeingPressed();
+    }
+
+    public boolean goBlue() {
+        return driver.bButton.isBeingPressed();
+    }
+
+    public boolean goGreen() {
+        return driver.xButton.isBeingPressed();
+    }
+
+    public boolean goWhite() {
+        return driver.yButton.isBeingPressed();
+    }
+
+    // operator controls
+    public boolean goPink() {
+        return operator.aButton.isBeingPressed();
+    }
+
+    public boolean goPurple() {
+        return operator.bButton.isBeingPressed();
+    }
+
+    public boolean goOrange() {
+        return operator.xButton.isBeingPressed();
+    }
+
+    public boolean goCyan() {
+        return operator.yButton.isBeingPressed();
     }
 }
